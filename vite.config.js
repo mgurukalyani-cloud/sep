a12889d3244f228b1cdcaa2846b8c76e3920126c
Guru Kalyani <mgurukalyani@gmail.com>
2026-09-22 07:21:08 +0530

    fix: change base path to sep

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/jashu/' : '/',
+  base: mode === 'production' ? '/sep/' : '/',
   server: {
     port: 5173,
     host: true
   }
 }));
-
